49f4fa9eed3f9ca75be29f7823192a42c14ead9d
Maurício Dionísio de Souza <dionmauricio@gmail.com>
2024-09-24 13:46:47 -0300

    feat: Implementação do modelo para banco

---
+++ Models/BancoModel.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+namespace IntegraBrasilApi.Models;
+
+public class BancoModel
+{
+    [JsonPropertyName("ispb")]
+    public string Ispb { get; set; } = string.Empty;
+
+    [JsonPropertyName("name")]
+    public string Nome { get; set; } = string.Empty;
+
+    [JsonPropertyName("code")]
+    public int Codigo { get; set; }
+
+    [JsonPropertyName("fullName")]
+    public string NomeCompleto { get; set; } = string.Empty;
+}
